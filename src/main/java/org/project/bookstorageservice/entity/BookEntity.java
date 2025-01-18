accdb0b29546739fd8d80bd0fb39e7b0a70d3318
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-18 12:34:44 +0300

    Book entity

--- src/main/java/org/project/bookstorageservice/entity/BookEntity.java
+++ src/main/java/org/project/bookstorageservice/entity/BookEntity.java
@@ -1,2 +1,32 @@
-package org.project.bookstorageservice.entity;public class BookEntity {
+package org.project.bookstorageservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String genre;
+    private String description;
+
+    @Column(nullable = false)
+    private String author;
 }
