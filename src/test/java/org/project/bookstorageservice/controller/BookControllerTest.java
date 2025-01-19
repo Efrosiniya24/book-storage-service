c8de958a112d19ba09b9a62937092d0754698ac3
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-19 14:49:54 +0300

    get all books test

--- src/test/java/org/project/bookstorageservice/controller/BookControllerTest.java
+++ src/test/java/org/project/bookstorageservice/controller/BookControllerTest.java
@@ -2,6 +2,7 @@ package org.project.bookstorageservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.Name;
 import org.junit.jupiter.api.Test;
 import org.project.bookstorageservice.dto.BookDTO;
 import org.project.bookstorageservice.service.BookService;
@@ -11,7 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,7 +34,7 @@ class BookControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void addBook() throws Exception {
+    void addBookTest() throws Exception {
         //given
         BookDTO bookDTO = new BookDTO(1L, "isbn", "name", "genre", "description", "author");
         String bookDTOJson = objectMapper.writeValueAsString(bookDTO);
@@ -43,5 +48,25 @@ class BookControllerTest {
                 .content(bookDTOJson))
                 .andExpect(status().isOk());
         verify(bookService, times(1)).addBook(bookDTO);
+    }
+
+    @Test
+    void getAllBooksTest() throws Exception {
+        //given
+        List<BookDTO> booksList = Arrays.asList(
+                new BookDTO(1L, "isbn", "name", "genre", "description", "author"),
+                new BookDTO(2L, "isbn2", "name2", "genre2", "description2", "author2")
+        );
+        String booksListJson = objectMapper.writeValueAsString(booksList);
+
+        //when
+        when(bookService.listOfBooks()).thenReturn(booksList);
+
+        //then
+        mockMvc.perform(get("/books/book-storage/all-books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(booksListJson))
+                .andExpect(status().isOk());
+        verify(bookService, times(1)).listOfBooks();
     }
 }
